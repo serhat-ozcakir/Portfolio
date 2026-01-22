8e2cf0431eeabd335ddb8a733e864c9dc5fa5b14
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-22 09:20:37 +0100

    first commit

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
+import { About } from './sections/about/about';
+import { Contact } from './sections/contact/contact';
+import { Friends } from './sections/friends/friends';
+import { Hero } from './sections/hero/hero';
+import { Skills } from './sections/skills/skills';
+import { Work } from './sections/work/work';
+
+@Component({
+  selector: 'app-home',
+  imports: [Header,Footer,About,Contact,Friends,Hero,Skills,Work],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
---
+++ src/app/pages/home/sections/friends/friends.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friends',
+  imports: [],
+  templateUrl: './friends.html',
+  styleUrl: './friends.scss',
+})
+export class Friends {
+
+}
