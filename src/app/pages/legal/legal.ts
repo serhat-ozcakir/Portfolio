3d33aba654f4e12f5294dc58401a7b0fc05b1c33
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-27 17:55:43 +0100

    legal page created and edited

--- src/app/pages/legal/legal.ts
+++ src/app/pages/legal/legal.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-legal',
-  imports: [],
+  imports: [Header,Footer],
   templateUrl: './legal.html',
   styleUrl: './legal.scss',
 })
