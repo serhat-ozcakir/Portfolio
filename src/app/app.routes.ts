88ec349ce57f422c3d22e338ddd62dec90756c11
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-28 16:51:50 +0100

    Route  established

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Privacy } from './pages/privacy/privacy';
+import { Legal } from './pages/legal/legal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: Home
+    },
+    {
+        path:'privacy',
+        component: Privacy
+    },
+    {
+        path:'legal',
+        component: Legal
+    },
+     { path: '**', 
+       redirectTo: '' }
+    
+];
