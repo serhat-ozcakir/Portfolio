0a12101c6e24a45404577b9c1389db3c6d6f5683
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-28 13:02:26 +0100

    privacy page added and created

--- src/app/pages/privacy/privacy.ts
+++ src/app/pages/privacy/privacy.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../shared/footer/footer';
+import { Header } from '../../shared/header/header';
 
 @Component({
   selector: 'app-privacy',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './privacy.html',
   styleUrl: './privacy.scss',
 })
