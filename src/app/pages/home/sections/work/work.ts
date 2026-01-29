18dddeba6367b672b2e76672e81cc13cec8d7ffc
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-29 12:44:31 +0100

    work sectıon added and edıted

--- src/app/pages/home/sections/work/work.ts
+++ src/app/pages/home/sections/work/work.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './work.scss',
 })
 export class Work {
+  gitHub(){
+    window.open("https://github.com/serhat-ozcakir", "_blank");
+  }
+  
+  elPolo(){
+    window.open("https://oezcakir-serhat.developerakademie.net/El%20Pollo%20Locco/index.html", "_blank");
+  }
 
+  pokedex(){
+    window.open("https://oezcakir-serhat.developerakademie.net/Pokedex/index.html", "_blank");
+  }
 }
