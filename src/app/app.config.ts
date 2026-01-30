73498b959cec109b80c58332ca4341e9b6d1870c
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-30 08:57:40 +0100

    ng translate structure added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from "@angular/common/http";
+import { provideTranslateService, TranslateService } from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,15 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'en',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      })
+    }),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Home],
+  imports: [RouterOutlet,Home,TranslatePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('project-portfolio');
+  
+  private translate = inject(TranslateService);
+  useLanguage(language: string): void {
+      this.translate.use(language);
+  }
+
 }
