5b0a49a3b112f1c8c23e40f55994ded0fa0359ce
serhat özcakir <serhatozcakir35@gmail.com>
2026-01-30 10:08:35 +0100

    Legal and Privacy pages historyBack() function added

--- src/app/pages/legal/legal.ts
+++ src/app/pages/legal/legal.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
+import { TranslateModule } from '@ngx-translate/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-legal',
-  imports: [Header,Footer],
+  imports: [Header,Footer,TranslateModule],
   templateUrl: './legal.html',
   styleUrl: './legal.scss',
 })
 export class Legal {
-
+    constructor(private location: Location) { }
+    historyBack(): void {
+    this.location.back();
+  }
 }
--- src/app/pages/privacy/privacy.ts
+++ src/app/pages/privacy/privacy.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../shared/footer/footer';
 import { Header } from '../../shared/header/header';
+import { TranslateModule } from '@ngx-translate/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslateModule],
   templateUrl: './privacy.html',
   styleUrl: './privacy.scss',
 })
 export class Privacy {
-
+  constructor(private location: Location) { }
+  historyBack(): void {
+    this.location.back();
+  }
 }
